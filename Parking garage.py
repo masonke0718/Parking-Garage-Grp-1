dac55ee630b3cb3f0c20728c41b4fb7035d15bad
masonke0718 <72037412+masonke0718@users.noreply.github.com>
2020-10-12 04:30:40 -0500

    Add files via upload

--- Parking garage.py
+++ Parking garage.py
@@ -6,33 +6,34 @@ class ParkingGarage():
         self.currentTickets = currentTickets
     def enteringGarage(self):
         lok = len(list(self.currentTickets.keys()))
-        lot = len(self.tickets)
-        if lok == lot:
+        if lok == 10:
             print("Garage full")
-        elif lok != lot:
+        elif lok != 10:
             poped = self.tickets.pop()
             print(f"Welcome to our Garage.  Here's your ticket #: {poped}")
             self.currentTickets[poped] = ""
             self.parkingSpaces.pop()
     def exitingGarage(self):
-        exitTicketNumber = int(input("What's your ticket #? Or type 'quit' to exit: "))
+        exitTicketNumber = int(input("What's your ticket #? "))
+        if exitTicketNumber in list(self.currentTickets.keys()):
+            print("We found your ticket.")
+        else:
+            print("Sorry, we could not find that ticket number") 
         for currentTicket in list(self.currentTickets.keys()):
-           if currentTicket == exitTicketNumber:
-                pay = input("We found your ticket. Please pay now by typing 'pay' or type 'quit' to exit.")
+            if currentTicket == exitTicketNumber:
+                pay = input("Please pay now by typing 'pay' or type 'quit' to exit.")
                 if pay.lower() == 'pay':
                     self.currentTickets[currentTicket] = "Paid"   # Updating Status of dictionary to paid
                     self.currentTickets.pop(currentTicket)
                     self.tickets.append(currentTicket)
                     self.parkingSpaces.append(currentTicket)
-                    print("Thank You. Have a nice day!")
+                    print("Thank You. You have 15 minutes to exit the garage. Have a nice day!")
                 elif pay.lower() == 'quit':
                     print("That's fine.  But you're now stuck in the garage. Get comfortable")
                     break
-                
-        if exitTicketNumber != currentTicket:
+                elif currentTicket != exitTicketNumber:
                     print("Sorry, we could not find that ticket number")
-                    
-        
+                     
 parking = ParkingGarage([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10],{})  #INSTANTIATING here
 def run():
     while True:
